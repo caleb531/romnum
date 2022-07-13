b129f921fa46e4e7f37816eff3cdc31fa2c98111
Caleb Evans <caleb@calebevans.me>
2022-07-13 15:37:55 -0700

    Migrate from nose to nose2 with coverage

--- test_romnum.py
+++ test_romnum.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import json
-import nose.tools as nose
+import unittest
 
 from romnum import encode, decode, RomanNumeralException
 
@@ -15,35 +15,35 @@ def get_decoding_test_cases():
     return {value: key for key, value in get_encoding_test_cases().items()}
 
 
-class TestEncode(object):
+class TestEncode(unittest.TestCase):
 
     def test_encode(self):
         for integer, romnum in get_encoding_test_cases().items():
-            yield nose.assert_equal, encode(int(integer)), romnum
+            yield self.assertEqual, encode(int(integer)), romnum
 
     def test_invalid_integer(self):
-        with nose.assert_raises(RomanNumeralException):
+        with self.assertRaises(RomanNumeralException):
             encode('abc')
 
 
-class TestDecode(object):
+class TestDecode(unittest.TestCase):
 
     def test_decode(self):
         for romnum, integer in get_decoding_test_cases().items():
-            yield nose.assert_equal, decode(romnum), int(integer)
+            yield self.assertEqual, decode(romnum), int(integer)
 
     def test_invalid_order(self):
-        with nose.assert_raises(RomanNumeralException):
+        with self.assertRaises(RomanNumeralException):
             decode('CMMVII')
 
     def test_invalid_repetition(self):
-        with nose.assert_raises(RomanNumeralException):
+        with self.assertRaises(RomanNumeralException):
             decode('CXXXXII')
 
     def test_invalid_case(self):
-        with nose.assert_raises(RomanNumeralException):
+        with self.assertRaises(RomanNumeralException):
             decode('mmxviii')
 
     def test_invalid_characters(self):
-        with nose.assert_raises(RomanNumeralException):
+        with self.assertRaises(RomanNumeralException):
             decode('xyz')
